c0a5abbb1fa88ff389e09e2f50b597c7aeb22205
StarProxima <maks.prozorov1@yandex.ru>
2023-09-13 02:09:02 +0300

    Task 4

---
+++ LR1/task4.py
@@ -0,0 +1,26 @@
+import cv2
+
+def copyVideo():
+    video = cv2.VideoCapture(r'.\LR1\src\video.mp4', cv2.CAP_ANY)
+    ok, vid = video.read()
+
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # type: ignore
+    video_writer = cv2.VideoWriter("./LR1/Output/video_copy.mp4", fourcc, 25, (w, h))
+    while (True):
+        ok, vid = video.read()
+        if not ok: break
+
+        # cv2.imshow('Video', vid)
+        video_writer.write(vid)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+copyVideo()
