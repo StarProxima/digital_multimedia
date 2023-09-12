1bba8be1b1dcf1f544d2059f2461b87a7e1039da
StarProxima <maks.prozorov1@yandex.ru>
2023-09-13 02:37:20 +0300

    Task 9

---
+++ LR1/task9.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ok, frame = cap.read()
+    if not ok:
+        break
+
+    cv2.imshow("camera", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
