3b0fd736f84d26896f27076ae4cba81b6788b1bf
StarProxima <maks.prozorov1@yandex.ru>
2023-09-13 02:30:51 +0300

    Task 7

---
+++ LR1/task7.py
@@ -0,0 +1,26 @@
+import cv2
+
+def readIPWriteTOFile():
+    video = cv2.VideoCapture(0)
+    ok, vid = video.read()
+
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # type: ignore
+    video_writer = cv2.VideoWriter("./LR1/Output/recorded_video.mp4", fourcc, 25, (w, h))
+
+    while (True):
+        ok, vid = video.read()
+
+        cv2.imshow('Video', vid)
+        video_writer.write(vid)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+readIPWriteTOFile()
